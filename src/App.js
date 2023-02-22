7cc5fe85b6a5c1829261173e929c4acc7c4a2ef3
Ifiokobong Akpan <iakpan916@gmail.com>
2023-02-22 13:53:49 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import Footer from './Footer';
+import Loading from './Loading';
+import Tours from './Tours';
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project';
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <React.Fragment>
+      <main>
+        <Tours tours={tours} />
+      </main>
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className='footer-credit'>
+      <p>Copyright © 2023 Ifiokobong Akpan</p>
+    </div>
+  );
+};
+
+export default Footer;
+
+// create a footer with full width html and css
